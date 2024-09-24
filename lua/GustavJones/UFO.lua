b385c4be678b6e5b975eb537efa2d2b625b2aba4
GustavJones <gustavjones3@gmail.com>
2024-09-24 12:32:30 +0000

    Add new eol visuals

--- lua/GustavJones/UFO.lua
+++ lua/GustavJones/UFO.lua
@@ -13,14 +13,41 @@ return {
 		vim.keymap.set("n", "<leader>cp", "zc", { noremap = true, silent = true, desc = "Close fold" })
 		vim.keymap.set("n", "<leader>cP", "zM", { noremap = true, silent = true, desc = "Close all folds" })
 
-		-- Option: treesitter as a main provider instead
-		-- (Note: the `nvim-treesitter` plugin is *not* needed.)
-		-- ufo uses the same query files for folding (queries/<lang>/folds.scm)
-		-- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
+		local handler = function(virtText, lnum, endLnum, width, truncate)
+			local newVirtText = {}
+			local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+			local sufWidth = vim.fn.strdisplaywidth(suffix)
+			local targetWidth = width - sufWidth
+			local curWidth = 0
+			for _, chunk in ipairs(virtText) do
+				local chunkText = chunk[1]
+				local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+				if targetWidth > curWidth + chunkWidth then
+					table.insert(newVirtText, chunk)
+				else
+					chunkText = truncate(chunkText, targetWidth - curWidth)
+					local hlGroup = chunk[2]
+					table.insert(newVirtText, { chunkText, hlGroup })
+					chunkWidth = vim.fn.strdisplaywidth(chunkText)
+					-- str width returned from truncate() may less than 2nd argument, need padding
+					if curWidth + chunkWidth < targetWidth then
+						suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+					end
+					break
+				end
+				curWidth = curWidth + chunkWidth
+			end
+			table.insert(newVirtText, { suffix, "MoreMsg" })
+			return newVirtText
+		end
+
 		require("ufo").setup({
-			provider_selector = function(bufnr, filetype, buftype)
-				return { "treesitter", "indent" }
-			end,
+			fold_virt_text_handler = handler,
+
+			-- -- For Treesitter provider
+			-- provider_selector = function(bufnr, filetype, buftype)
+			-- 	return { "treesitter", "indent" }
+			-- end,
 		})
 	end,
 }
