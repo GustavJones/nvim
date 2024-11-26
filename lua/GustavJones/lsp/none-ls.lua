b8cfdd14804b462b5535fcd09b88746bf8577d2a
GustavJones <gustavjones3@gmail.com>
2024-11-26 17:41:48 +0000

    Remove Autoformat

--- lua/GustavJones/lsp/none-ls.lua
+++ lua/GustavJones/lsp/none-ls.lua
@@ -24,6 +24,19 @@ return {
 			},
 		})
 
+		local opts = { noremap = true, silent = true }
+		local keymap = vim.keymap
+
+		opts.desc = "Format Document"
+		keymap.set("n", "cf", function()
+			vim.lsp.buf.format({
+				filter = function(client)
+					--  only use null-ls for formatting instead of lsp server
+					return client.name == "null-ls"
+				end,
+			})
+		end, opts) -- show definition, references
+
 		-- for conciseness
 		local formatting = null_ls.builtins.formatting -- to setup formatters
 		local diagnostics = null_ls.builtins.diagnostics -- to setup linters
@@ -49,22 +62,22 @@ return {
 			},
 			-- configure format on save
 			on_attach = function(current_client, bufnr)
-				if current_client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({
-								filter = function(client)
-									--  only use null-ls for formatting instead of lsp server
-									return client.name == "null-ls"
-								end,
-								bufnr = bufnr,
-							})
-						end,
-					})
-				end
+				-- if current_client.supports_method("textDocument/formatting") then
+				-- 	vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+				-- 	vim.api.nvim_create_autocmd("BufWritePre", {
+				-- 		group = augroup,
+				-- 		buffer = bufnr,
+				-- 		callback = function()
+				-- 			vim.lsp.buf.format({
+				-- 				filter = function(client)
+				-- 					--  only use null-ls for formatting instead of lsp server
+				-- 					return client.name == "null-ls"
+				-- 				end,
+				-- 				bufnr = bufnr,
+				-- 			})
+				-- 		end,
+				-- 	})
+				-- end
 			end,
 		})
 	end,
