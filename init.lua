5582dd3e6566bf5f3e1e98680f961d356e575b06
GustavJones <gustavjones3@gmail.com>
2025-01-11 12:09:01 +0200

    Remove Line Wrapping

--- init.lua
+++ init.lua
@@ -9,6 +9,7 @@ vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
+vim.opt.wrap = false
 
 vim.opt.clipboard:append("unnamedplus")
 -- vim.opt.swapfile = false
